b61ac022a4d9c5dcadc44b1c66ee0750ab95ba28
Hamza Khawaja <mhamza@xavor.com>
2022-03-31 15:17:00 +0500

    added_reducer_logic

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,59 @@
+import React, { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const CartProvider = (props) => {
-  const addItemToCartHandler = () => {};
+const defaultCartState = {
+  items: [],
+  totalAmount: 0,
+};
 
-  const removeItemFromCartHandler = () => {};
+//cartReducer has intial state snapshot, and the action.
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+      const updatedItems = state.items.concat(action.item);
+      const updatedTotalAmount =
+        state.totalAmount + action.item.price * action.item.amount;
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+    default:
+      return defaultCartState;
+  }
+
+  return defaultCartState;
+};
+
+const CartProvider = (props) => {
+  //useReducer returns an array with two elements:
+  //i) state snapshot after dispatch function has executed.
+  //ii) dispatch function that tells when to execute a function.
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+
+  const addItemToCartHandler = (item) => {
+    // 'type' is an identifier that lets you know which what to execute
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+
+  const removeItemFromCartHandler = (id) => {
+    dispatchCartAction({ type: "REMOVE", id: id });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addItem: addItemToCartHandler,
     removeItem: removeItemFromCartHandler,
   };
 
-  return <CartContext.Provider value={cartContext}>{props.children}</CartContext.Provider>;
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
+    </CartContext.Provider>
+  );
 };
 
 export default CartProvider;
