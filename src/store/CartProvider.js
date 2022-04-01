b6d7022b69b571cf138c26683117619884530538
Hamza Khawaja <mhamza@xavor.com>
2022-04-01 13:58:57 +0500

    remove_functionality

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,20 +8,77 @@ const defaultCartState = {
 
 //cartReducer has intial state snapshot, and the action.
 const cartReducer = (state, action) => {
+  let updatedItems;
+  let updatedTotalAmount;
+  let existingCartItemIndex;
+  let existingCartItem;
+
   switch (action.type) {
     case "ADD":
-      const updatedItems = state.items.concat(action.item);
-      const updatedTotalAmount =
+      //find whether item already exists in the array.
+      existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.item.id
+      );
+
+      //fetch that cart item
+      existingCartItem = state.items[existingCartItemIndex];
+
+      if (existingCartItem) {
+        //update the amount of that item
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount + action.item.amount,
+        };
+
+        //get all state items in another list
+        updatedItems = [...state.items];
+        //replace existing item with updated item with new data
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        updatedItems = state.items.concat(action.item);
+      }
+
+      //   const updatedItems = [...state.items, action.item];
+      updatedTotalAmount =
         state.totalAmount + action.item.price * action.item.amount;
       return {
         items: updatedItems,
         totalAmount: updatedTotalAmount,
       };
+
+    case "REMOVE":
+      //find whether item already exists in the array.
+      existingCartItemIndex = state.items.findIndex(
+        (item) => item.id === action.id
+      );
+
+      //fetch that cart item
+      existingCartItem = state.items[existingCartItemIndex];
+
+      //remove total removed item from the totalAmount of items in the cart.
+      updatedTotalAmount = state.totalAmount - existingCartItem.amount;
+
+      if (existingCartItem.amount === 1) {
+        updatedItems = state.items.filter((item) => item.id !== action.id);
+      } else {
+        const updatedItem = {
+          ...existingCartItem,
+          amount: existingCartItem.amount - 1,
+        };
+        updatedItems = [...state.items];
+        updatedItems[existingCartItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
+        totalAmount: updatedTotalAmount,
+      };
+
     default:
       return defaultCartState;
   }
 
-//   return defaultCartState;
+  //   return defaultCartState;
 };
 
 const CartProvider = (props) => {
@@ -34,7 +91,7 @@ const CartProvider = (props) => {
   );
 
   const addItemToCartHandler = (item) => {
-    // 'type' is an identifier that lets you know which what to execute
+    // 'type' is an identifier that lets you know what to execute
     dispatchCartAction({ type: "ADD", item: item });
   };
 
@@ -42,6 +99,7 @@ const CartProvider = (props) => {
     dispatchCartAction({ type: "REMOVE", id: id });
   };
 
+  //THIS IS THE OBJECT THAT IS BEING PASSED TO ALL THE ELEMENTS OF THE APP.
   const cartContext = {
     items: cartState.items,
     totalAmount: cartState.totalAmount,
